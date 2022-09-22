4c3a47271f0b5709c66f7b9841b77a7503c79c2e
Aditya Shaw <adityashaw.slds@gmail.com>
2022-09-23 01:54:43 +0530

    add a state to detect when searchbox is focused/clicked

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+
 import Search from './Search';
 
 function Main() {
+  const [searchFocused, setSearchFocused] = useState(false);
+
   return (
 
     <main className="col-8 col-md-9 ms-auto col-lg-10">
-      <Search />
-      <section>
-        <img className="w-100" src="/assets/img/hero-image.png" alt="Hero" />
-        <img className="w-100" src="/assets/img/clients.png" alt="Client" />
-        <img className="w-100" src="/assets/img/feedback.png" alt="Customer Feedback" />
+      <Search
+        searchFocused={searchFocused}
+        setSearchFocused={setSearchFocused}
+      />
+      <section
+        className={searchFocused ? "collapse" : ""}
+      >
+        <img
+          className="w-100"
+          src="/assets/img/hero-image.png" alt="Hero"
+        />
+        <img
+          className="w-100"
+          src="/assets/img/clients.png" alt="Client"
+        />
+        <img
+          className="w-100"
+          src="/assets/img/feedback.png" alt="Customer Feedback"
+        />
       </section>
     </main>
   );
