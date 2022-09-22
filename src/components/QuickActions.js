890235ab06b51698422c764cdaeabaed9480aecd
Aditya Shaw <adityashaw.slds@gmail.com>
2022-09-23 02:00:14 +0530

    fix typo

--- src/components/QuickActions.js
+++ src/components/QuickActions.js
@@ -2,7 +2,7 @@
 function QuickActions() {
   return (
     <div>
-      <h6 className="fw-bold mb-4">Track new accounts</h6>
+      <h6 className="fw-bold mb-4">Track new account</h6>
       <h6 className="fw-bold mb-4">Bulk track accounts</h6>
       <h6 className="fw-bold mb-4">Manage accounts</h6>
     </div>
